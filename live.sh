0257e50cdc251d424e3f86ee8c1a1d30dfd375b7
yuriZz3ddy <132253210+yuriZz3ddy@users.noreply.github.com>
2024-01-22 23:59:15 -0400

    Delete live.sh

--- live.sh
+++
@@ -1,1 +0,0 @@
-ffmpeg -stream_loop -1 -re -i video.mp4 -stream_loop -1 -re -i http://fr1.1mix.co.uk:8060/192b -vcodec libx264 -pix_fmt yuvj420p -maxrate 20048k -preset veryfast -r 12 -framerate 30 -g 50 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150000k -f flv  rtmp://a.rtmp.youtube.com/live2/75ms-d3zz-buj8-2143-em4dppo
