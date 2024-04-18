1730e687d1c99e4038d615f7b2bcbe3633d80ddf
yuriZz3ddy <132253210+yuriZz3ddy@users.noreply.github.com>
2024-04-18 04:54:09 -0400

    Update imagex.js

--- imagex.js
+++ imagex.js
@@ -4,8 +4,8 @@ fetch("https://japi.rest/discord/v1/user/933891035216613446", {
 //acima discord Webhook bot JSON usuário
   .then(response => response.json())
   .then(data => {
-    var uid = data.user.id
-    var avatar = data.user.avatar
+    var uid = data.data.id
+    var avatar = data.data.avatar
     const image = "https://cdn.discordapp.com/avatars/" +`${uid}` + "/" + `${avatar}` + ".webp?size=2048";
     img1.src = image;
     img1.style.borderRadius = "50px";
